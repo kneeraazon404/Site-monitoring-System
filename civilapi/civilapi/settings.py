6a19053d11e9f5ae363ad3bd1566848492b6c50e
kneeraazon01 <karkinirajan1999@gmail.com>
2020-09-05 17:35:12 +0545

    database upgraded to postgres

--- civilapi/civilapi/settings.py
+++ civilapi/civilapi/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = "civilapi.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "civilapi",
+        "USER": "postgres",
+        "PASSWORD": "karki582465",
+        "HOST": "localhost",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
