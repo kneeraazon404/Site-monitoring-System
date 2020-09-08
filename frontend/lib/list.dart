2bbd6e845da00f233bf475966ff2440d845e553b
kneeraazon01 <karkinirajan1999@gmail.com>
2020-09-08 12:14:46 +0545

    lets go

--- frontend/lib/list.dart
+++ frontend/lib/list.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 import './user.dart';
 import './detail.dart';
+
 class MyList extends StatefulWidget {
   MyList({Key key}) : super(key: key);
 
@@ -13,7 +14,7 @@ class MyList extends StatefulWidget {
 
 class _MyListState extends State<MyList> {
   List<User> users = [];
-  Future getAll() async{
+  Future getAll() async {
     var data = await http.get("http://localhost:1337/apis/");
     var jsonData = json.decode(data.body);
 
@@ -22,31 +23,36 @@ class _MyListState extends State<MyList> {
     }
     return users;
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: FutureBuilder(
-         future: getAll(),
-         builder: (BuildContext context,AsyncSnapshot snapshot){
-           if(snapshot.data == null){
-             return Container(child:Center(
-               child: Text("Loading..."),
-             ));
-           }else{
-           return ListView.builder(
-             itemCount: snapshot.data.length,
-             itemBuilder: (BuildContext context,int index){
-               return ListTile(
-                 title: Text(snapshot.data[index].name),
-                 subtitle: Text(snapshot.data[index].email),
-                 onTap: (){
-                   Navigator.push(context,new MaterialPageRoute(builder: (context)=>MyDetail(snapshot.data[index])));
-                 },
-                 );
-              }
-             );}
-         }
-         ),
+      child: FutureBuilder(
+          future: getAll(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                  child: Center(
+                child: Text("Loading..."),
+              ));
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(snapshot.data[index].name),
+                      subtitle: Text(snapshot.data[index].email),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                                builder: (context) =>
+                                    MyDetail(snapshot.data[index])));
+                      },
+                    );
+                  });
+            }
+          }),
     );
   }
 }
