ac4e6e960556814a15e83b5d46c0421416be9da5
kneeraazon01 <karkinirajan1999@gmail.com>
2021-10-04 02:35:00 +0545

    time zone and code updated

--- drfapi/settings.py
+++ drfapi/settings.py
@@ -116,12 +116,6 @@ LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
 
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
